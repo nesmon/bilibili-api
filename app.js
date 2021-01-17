c36d7e62f40ff19df25d109dc1e12789ed8f41d2
Nenes <Nenes@DESKTOP-3TSN9T7>
2021-01-17 18:06:23 +0100

    Return to export rather than class

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
-const api = new apiRouter()
 
 const app = express();
 
@@ -23,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'api')));
 
 
 app.use('/', indexRouter);
-app.use('/api', apiRouter);
+app.use(`/api`, apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,19 @@
 const express = require('express');
-const router = express.Router();
+const router = new express.Router()
 const {promisify} = require("util");
 const readdir = promisify(require("fs").readdir);
 
-class apiRoute {
-  constructor(client) {
-    this.client = client;
-  }
 
+/* GET users listing. */
+router.get('/bilibili', async function(req, res, next) {
+  res.set('Content-Type', 'application/json')
 
-  route () {
-    /* GET users listing. */
-    router.get('/bilibili', async function(req, res, next) {
-      res.set('Content-Type', 'application/json')
+  const evtFiles = await readdir("./api/bilibili/");
+  console.log(`Loading a total of ${evtFiles.length} bilibili picture.`);
 
-      const evtFiles = await readdir("./api/bilibili/");
-      console.log(`Loading a total of ${evtFiles.length} bilibili picture.`);
+  res.send(JSON.stringify({
+    data : "bilibili/" + evtFiles[Math.floor(Math.random() * Math.floor(evtFiles.length))]
+  }));
+});
 
-      res.send(JSON.stringify({
-        data : "bilibili/" + evtFiles[Math.floor(Math.random() * Math.floor(evtFiles.length))]
-      }));
-    });
-  }
-
-}
-
-module.exports = new apiRoute();
+module.exports = router;
